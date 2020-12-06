b2c49086350871f6cfc221a4156549d7876c07da
geekrhibi <geekrhibi@gmail.com>
2020-12-06 12:10:21 +0100

    skill v2

--- controllers/skill.js
+++ controllers/skill.js
@@ -2,22 +2,28 @@ const router = require('express').Router() ;
 const User = require('../models/user');
 const Skill = require('../models/skill');
 const notification = require('../models/notification');
+const skill = require('../models/skill');
 
 
 exports.addSkill = async (req,res) =>{
     try {
         
-        const skills = req.params.skills; 
+        if (req.userData.user.role!='ROLE_ADMIN')
+            return res
+                .status(401)
+                .json({message : 'only admin can add skills '})
+
+        const skills = req.body.skills; 
         if (!skills){
             return res
             .status(409)
             .json({ message: "SKill  doesn't  exist ! " });
         }
         skills.forEach(async element => {
-            const skill = await Skill.create({
-                element 
+            const addedSkills = await Skill.create({
+                name : element 
             });
-            await skill.save();    
+            await addedSkills.save();    
         });
 
         res.json(skills)
@@ -44,19 +50,13 @@ exports.getSkills = async (req,res) =>{
 
 exports.deleteSkill = async (req,res) =>{
     try {
-        const skill = await Skill.findById(req.params.skillId); 
+        const skillObject = await Skill.findById(req.params.skillId); 
         
-        if (!skill) {
+        if (!skillObject) {
             return res
             .status(409)
             .json({ message: "SKill  doesn't  exist ! " });
         }
-
-        
-        // const users = await User.find({
-        //     skills : {"$in" : [req.params.skillId]}
-        // })
-
         await User.updateMany(
             {},
             {$pull : { skills : {$in : req.params.skillId } } },
